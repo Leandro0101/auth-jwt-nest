98948ed4b6c6c58c261fd81aa834e4659d38cfc0
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-07 14:22:52 -0300

    feat: add login at AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './services/auth.service';
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './shared/localStrategy';
+import { JwtStrategy } from './shared/jwt.strategy';
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController, AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
---
+++ src/auth/shared/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: '78125cdda3bae5f94b5930b42286ee97',
+};
